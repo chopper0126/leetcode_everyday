e16995bfba02b9fdd73d17425b3257fdace4d709
Chopper <342274512@qq.com>
2023-06-11 19:31:56 +0800

    add lt everyday

---
+++ src/main/java/P_23年6月/ListNode.java
@@ -0,0 +1,18 @@
+package P_23年6月;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/P_23年6月/T_6_11.java
@@ -0,0 +1,33 @@
+package P_23年6月;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class T_6_11 {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(-1);
+
+        System.out.println(removeZeroSumSublists(head));
+    }
+    public static ListNode removeZeroSumSublists(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        Map<Integer,ListNode> map = new HashMap<>();
+        int sum = 0;
+        for(ListNode node = dummy;node != null;node = node.next){
+            sum += node.val;
+            map.put(sum,node);//如果出现重复的key，put会覆盖
+        }
+        sum = 0;
+        for (ListNode node = dummy;node != null;node = node.next){
+            sum += node.val;
+            node.next = map.get(sum).next;
+        }
+        return dummy.next;
+    }
+
+}
+
+
+
